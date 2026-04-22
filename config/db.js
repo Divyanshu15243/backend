10c21f4d8da69792b24a635357aa68e2e16920e7
Divyanshu Patel <divyanshupatel15243@gmail.com>
2026-04-22 16:27:38 +0530

    login issue solved

--- config/db.js
+++ config/db.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 5000,
+      monitorCommands: false,
+    });
+    mongoose.set("debug", false);
     console.log("mongodb connection success!");
   } catch (err) {
     console.log("mongodb connection failed!", err.message);
