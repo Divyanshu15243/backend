26e798c8f24c559778c96da18f629d200e4bbef5
Divyanshu Patel <divyanshupatel15243@gmail.com>
2026-05-05 14:50:10 +0530

    cked chng

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
-const path = require("path");
 // const http = require("http");
 // const { Server } = require("socket.io");
 
@@ -27,8 +26,23 @@ const { isAuth, isAdmin } = require("../config/auth");
 //   getStoreCustomizationSetting,
 // } = require("../lib/notification/setting");
 
-connectDB();
 const app = express();
+
+let isConnected = false;
+const connect = async () => {
+  if (isConnected) return;
+  await connectDB();
+  isConnected = true;
+};
+
+app.use(async (req, res, next) => {
+  try {
+    await connect();
+    next();
+  } catch (err) {
+    res.status(500).json({ error: "Database connection failed" });
+  }
+});
 
 // We are using this for the express-rate-limit middleware
 // See: https://github.com/nfriedly/express-rate-limit
@@ -80,21 +94,9 @@ app.use((err, req, res, next) => {
   res.status(400).json({ message: err.message });
 });
 
-// Serve static files from the "dist" directory
 app.use("/static", express.static("public"));
 
-// Serve the index.html file for all routes
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
-const PORT = process.env.PORT || 5000;
-
-// const server = http.createServer(app);
-
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
-
-// app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+module.exports = app;
 
 // set up socket
 // const io = new Server(server, {
